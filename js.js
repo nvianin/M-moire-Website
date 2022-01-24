04e0ae61011cfcddc5497ef91d53392a1ebf6560
nvianin <31324783+nvianin@users.noreply.github.com>
2022-01-24 21:41:39 +0100

    update index.html, js.js, room.js and style.css

--- js.js
+++ js.js
@@ -1,20 +1,82 @@
+let log = console.log;
+Math.Clamp = (val, min, max) => {
+    return Math.min(Math.max(val, min), max)
+}
+
 let canvas, ctx;
 let rooms = []
+let scale = 1;
 
 window.onload = () => {
     canvas = document.querySelector("canvas")
     ctx = canvas.getContext("2d");
+    ctx.lineCap = "square"
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 
-    for (let i = 0; i < 2; i++) {
-        rooms.push(new Room(400 * Math.random(), 400 * Math.random(), Math.random() * 200, Math.random() * 200));
+    for (let i = 0; i < 80; i++) {
+        rooms.push(
+            new Room(
+                innerWidth * 3 * Math.random(),
+                innerHeight * 3 * Math.random(),
+                Math.random() * 300 + 100,
+                Math.random() * 300 + 100
+            ));
+    }
+
+
+    render()
+}
+let render = () => {
+    ctx.clearRect(0, 0, innerWidth, innerHeight);
+    requestAnimationFrame(render);
+    for (let i = 0; i < rooms.length; i++) {
+        /* for (let j = 0; j < rooms.length; j++) {
+            if (rooms[j].aabb(rooms[i])) {
+                rooms[i].color = "red"
+                rooms[j].color = "red"
+            }
+        } */
+        rooms[i].draw(ctx);
     }
 }
-
-let render = () => {
-    requestAnimationFrame(render);
-    for (room of rooms) {
-        room.draw(ctx);
+let mousedown = false;
+let startPos = {
+    x: 0,
+    y: 0
+}
+let offset = {
+    x: 0,
+    y: 0
+}
+window.onmousedown = e => {
+    canvas.style.cursor = "grabbing"
+    mousedown = true;
+    startPos.x = e.clientX - offset.x;
+    startPos.y = e.clientY - offset.y;
+}
+window.onmouseup = e => {
+    offset.x = e.clientX - startPos.x;
+    offset.y = e.clientY - startPos.y;
+    canvas.style.cursor = "grab"
+    mousedown = false;
+}
+window.onmousemove = e => {
+    if (mousedown) {
+        for (let r of rooms) {
+            r.offset.x = e.clientX - startPos.x;
+            r.offset.y = e.clientY - startPos.y;
+        }
     }
+}
+window.onwheel = e => {
+    /*  scale = Math.Clamp(scale - e.deltaY * .0001, .4, 5)
+     log(scale)
+     for (let r of rooms) {
+         r.scale = scale;
+     } */
+}
+window.onresize = () => {
+    canvas.width = innerWidth;
+    canvas.height = innerHeight;
 }
--- room.js
+++ room.js
@@ -1,40 +1,82 @@
+let lorem = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+Why do we use it?
+It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).`
+
 class Room {
     constructor(x, y, w, h) {
         this.x = x;
         this.y = y;
+
+        this.offset = {
+            x: 0,
+            y: 0
+        }
+        this.scale = 1;
 
         this.width = w;
         this.height = h;
 
         this.points = [];
 
-        for (var i = 0; i < 5; i++) {
-            let start = {};
-            start.x = Math.random() * window.innerWidth;
-            start.y = Math.random() * window.innerHeight;
-            this.points.push({
-                x: start.x,
-                y: start.y
-            });
-            for (var j = 0; j < 3; j++) {
-                this.points.push({
-                    x: Math.random() * window.innerWidth * this.width + start.x,
-                    y: Math.random() * window.innerHeight * this.width + start.y
-                });
-            }
-        }
+        this.text = lorem;
+
+
+        this.points.push({
+            x: this.x,
+            y: this.y
+        });
+        this.points.push({
+            x: this.width + this.x,
+            y: this.y
+        });
+        this.points.push({
+            x: this.width + this.x,
+            y: this.height + this.y
+        });
+        this.points.push({
+            x: this.x,
+            y: this.height + this.y
+        });
+
+        this.color = "black"
+
 
     }
 
     draw(ctx) {
         ctx.beginPath()
-        ctx.strokeStyle = "black"
-        ctx.moveTo(this.points[0].x, this.points[0].y);
+        ctx.strokeStyle = this.color;
+        ctx.fillStyle = "white"
+        ctx.moveTo(
+            (this.points[0].x + this.offset.x) * this.scale,
+            (this.points[0].y + this.offset.y) * this.scale
+        );
         for (let point of this.points) {
-            ctx.lineTo(Math.random() * window.innerWidth, Math.random() * window.innerHeight);
+            ctx.lineTo(
+                (point.x + this.offset.x) * this.scale,
+                (point.y + this.offset.y) * this.scale
+            );
             ctx.stroke();
         }
-        ctx.lineTo(this.points[0].x, this.points[0].y);
+        ctx.lineTo(
+            (this.points[0].x + this.offset.x) * this.scale,
+            (this.points[0].y + this.offset.y) * this.scale
+        );
+        ctx.lineWidth = 6;
         ctx.stroke();
+        ctx.fill()
+        ctx.font = "20px Helvetica"
+        ctx.fillText(this.text, this.x + this.offset.x, this.y + this.offset.y);
+    }
+
+    aabb(other) {
+        if ((this.x > other.x && this.x < other.x + other.width) ||
+            (this.x + this.width > other.x && this.x + this.width < other.x + other.width)) {
+            if ((this.y > other.y && this.y < other.y + other.height) ||
+                (this.y + this.height > other.y && this.y + this.height < other.y + other.height)) {
+                return true;
+            }
+        }
     }
 }
